f192b803dab1da36f30ca9a9fe356caef5dae7d3
hardikkalkani11 <109730034+hardikkalkani11@users.noreply.github.com>
2022-08-03 10:56:09 +0530

    ad dubuntu

--- deploy_scripts/start.sh
+++ deploy_scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
-cd /home/ec2-user/calculator
+source /home/ubuntu/.bash_profile
+cd /home/ubuntu/calculator
 NODE_ENV=production npm start > /dev/null 2> /dev/null < /dev/null &
